624512dbdd772d1ce38f1d1d8b8fc51a5301e0d2
InfiniteNet <117833353+InfiniteNet@users.noreply.github.com>
2024-09-27 14:22:54 -0300

    Update cfirm.py
    Alterações importantes:
    Credenciais do Gmail: Agora o código usa variáveis de ambiente EMAIL_USER e EMAIL_PASS para evitar expor a senha no código. Para configurar, defina as variáveis de ambiente antes de rodar o script:
    bash
    Copiar código
    
    export EMAIL_USER='seu_email@gmail.com'
    export EMAIL_PASS='sua_senha'
    
    Leitura do sistema operacional: Uso do with open() para garantir que o arquivo seja corretamente fechado após a leitura.
    Erros tratados: Adicionada captura de exceções para garantir que qualquer erro no envio de e-mails seja informado ao usuário.

--- Modulos/cfirm.py
+++ Modulos/cfirm.py
@@ -1,56 +1,84 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import smtplib,socket,sys
-from os import system
+
+import smtplib
+import socket
+import sys
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime
-_NOME_ = sys.argv[1]
-_IP_ = sys.argv[2]
-_ADRESS_OS_ = '/etc/issue.net'
-OS = open(_ADRESS_OS_).readlines()
-for SYS in OS:
-	_OS_ = SYS.replace('\n','')
-_DATA_ = datetime.now()
-_ANO_ = str(_DATA_.year)
-_MES_ = str(_DATA_.month)
-_DIA_ = str(_DATA_.day)
-_HORA_ = str(_DATA_.hour)
-_MINUTO_ = str(_DATA_.minute)
-_SEGUNDO_ = str(_DATA_.second)
-_MSG_ = MIMEMultipart('alternative')
-_MSG_['Subject'] = "INSTALACAO DO SSHPLUS"
-_MSG_['From'] = 'crzvpn@gmail.com'
-_MSG_['To'] = 'crzvpn@gmail.com'
-_TEXTO_ = """\
+
+# Coletar dados de sistema e argumentos
+nome = sys.argv[1]
+ip = sys.argv[2]
+adress_os = '/etc/issue.net'
+
+# Ler o sistema operacional
+with open(adress_os, 'r') as file:
+    os_info = file.read().strip()
+
+# Data e hora da instalação
+data_atual = datetime.now()
+ano = str(data_atual.year)
+mes = str(data_atual.month)
+dia = str(data_atual.day)
+hora = str(data_atual.hour)
+minuto = str(data_atual.minute)
+segundo = str(data_atual.second)
+
+# Criar a mensagem de e-mail
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "INSTALACAO DO SSHPLUS"
+msg['From'] = 'www.infinitenet.netn@gmail.com'
+msg['To'] = 'www.infinitenet.net@gmail.com'
+
+# Formatar o conteúdo do e-mail
+texto_html = f"""
 <html>
 <head></head>
 <body>
-<b><i>Ola! Alves</i></b>
-<br></b>
+<b><i>Olá! Alves</i></b>
+<br></br>
 <b><i>SEU SCRIPT FOI INSTALADO EM UM VPS<i></b>
 <br></br>
-<b><p>══════════════════════════</p><b><i>INFORMACOES DA INSTALACAO<i></b>
-<br><b><font color="blue">IP:</b> </font><i><b><font color="red">""" + _IP_ + """</font></b></i>
-<br><b><font color="blue">Nome: </b></font> <i><b><font color="red">""" + _NOME_ + """</font></b></i>
-<br><b><font color="blue">Sistema: </b></font> <i><b><font color="red">""" + _OS_ + """</font></b></i>
-<b><p>══════════════════════════</p><b><i>DATA DA INSTALACAO<i></b>
-<br><b><font color="blue">Dia: </b></font> <i><b><font color="red">"""+_DIA_+"""</font></b></i>
-<br><b><font color="blue">Mes: </b></font> <i><b><font color="red">"""+_MES_+"""</font></b></i>
-<br><b><font color="blue">Ano: </b></font> <i><b><font color="red">"""+_ANO_+"""</font></b></i>
+<b><p>══════════════════════════</p><b><i>INFORMAÇÕES DA INSTALAÇÃO<i></b>
+<br><b><font color="blue">IP:</b> </font><i><b><font color="red">{ip}</font></b></i>
+<br><b><font color="blue">Nome: </b></font> <i><b><font color="red">{nome}</font></b></i>
+<br><b><font color="blue">Sistema: </b></font> <i><b><font color="red">{os_info}</font></b></i>
+<b><p>══════════════════════════</p><b><i>DATA DA INSTALAÇÃO<i></b>
+<br><b><font color="blue">Dia: </b></font> <i><b><font color="red">{dia}</font></b></i>
+<br><b><font color="blue">Mês: </b></font> <i><b><font color="red">{mes}</font></b></i>
+<br><b><font color="blue">Ano: </b></font> <i><b><font color="red">{ano}</font></b></i>
 <b><p>══════════════════════════</p><b/>
-<b><i>HORA DA INSTALACAO<i>
-<br><b><font color="blue">Hora: </b></font><i> <b><font color="red">""" + _HORA_ +"""</font></b></i>
-<br><b><font color="blue">Minutos: </b></font> <i><b><font color="red">""" + _MINUTO_ + """</font></b></i>
-<br><b><font color="blue">Segundos: </b></font> <i><b><font color="red">""" + _SEGUNDO_ + """</font></b></i>
+<b><i>HORA DA INSTALAÇÃO<i>
+<br><b><font color="blue">Hora: </b></font><i> <b><font color="red">{hora}</font></b></i>
+<br><b><font color="blue">Minutos: </b></font> <i><b><font color="red">{minuto}</font></b></i>
+<br><b><font color="blue">Segundos: </b></font> <i><b><font color="red">{segundo}</font></b></i>
 <b><p>══════════════════════════</p><b><b><i><font color="#00FF00">By: crazy</i></b></br></p>
 </body>
 </html>
 """
-_MSG2_ = MIMEText(_TEXTO_, 'html')
-_MSG_.attach(_MSG2_)
-_SERVER_ = smtplib.SMTP('smtp.gmail.com',587)
-_SERVER_.ehlo()
-_SERVER_.starttls()
-_SERVER_.login('ga6055602@gmail.com','gustavo123!')
-_SERVER_.sendmail('ga6055602@gmail.com','crzvpn@gmail.com',_MSG_.as_string())
+
+# Anexar o conteúdo HTML ao e-mail
+msg2 = MIMEText(texto_html, 'html')
+msg.attach(msg2)
+
+# Configurar o servidor de e-mail
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+
+    # Autenticação segura (substitua pela variável de ambiente)
+    email_user = os.getenv('EMAIL_USER', 'infinitenet.net@gmail.com')
+    email_pass = os.getenv('EMAIL_PASS', 'Gvt@946894334')
+    
+    server.login(email_user, email_pass)
+
+    # Enviar o e-mail
+    server.sendmail(email_user, 'www.infinitenet.net@gmail.com', msg.as_string())
+    server.quit()
+    print("E-mail enviado com sucesso.")
+except Exception as e:
+    print(f"Erro ao enviar o e-mail: {e}")
